9844b4b03395a0a832c71a88344275046a1d1959
creightonja <creightonja@gmail.com>
2015-09-18 20:18:18 -0700

    Reinitialize

---
+++ js/scripts.js
@@ -0,0 +1,126 @@
+//Declaring global variables
+var toppings = [];
+var listOfToppings = ["Pepperoni", "Sausage", "Ham", "Chicken", "Olive", "Onion", "Peppers", "Mushroom", "Tomatoes"];
+var selectedToppings = [];
+var thePizza = new Pizza(0,0,0);
+var toppingLi;
+
+//Setting up prototype object
+function Pizza(num, size, toppings){
+  this.num = num;
+  this.size = size;
+  this.toppings = toppings;
+};
+
+//Setters for properties
+Pizza.prototype.setNum = function(num){
+  this.num = num;
+  return this.num;
+};
+
+Pizza.prototype.setSize = function(size){
+  this.size = size;
+  return this.size;
+};
+
+Pizza.prototype.setToppings = function(toppings){
+  this.toppings = toppings;
+  return this.toppings;
+};
+
+//Generating description based on different grammar of list length
+Pizza.prototype.description = function(){
+  var theToppings = this.toppings;
+  if (theToppings.length > 2){
+    var lastTopping = theToppings.pop();
+    var joinToppings = theToppings.join(", ");
+    return this.num + " " + this.size + " pizza(s) with " + joinToppings + ", and " + lastTopping;
+  } else if (theToppings.length == 2){
+    return this.num + " " + this.size + " pizza(s) with " + theToppings[0] + " and " + theToppings[1];
+  } else {
+    return this.num + " " + this.size + " pizza(s) with " + theToppings[0];
+  }
+};
+
+//Calculating the cost of a pizza
+Pizza.prototype.cost = function(){
+  var toppingCount = this.toppings.length;
+  if (this.size == "Small"){
+    var sizeCost = 5.50;
+    var toppingCost = toppingCount * .75;
+  } else if (this.size == "Medium"){
+    var sizeCost = 8.50;
+    var toppingCost = toppingCount * 1.25;
+  } else {
+    var sizeCost = 11.50;
+    var toppingCost = toppingCount * 1.75;
+  }
+  var pizzaCost = sizeCost + toppingCost;
+  var total = pizzaCost * this.num;
+  total.toFixed(2);
+  return total;
+};
+
+//Generating an Li element for appending to the Document
+function displayTopping(topping){
+  var toppingLi = "<li class='" + topping + " topping'>" + topping + "</li>";
+  return toppingLi;
+};
+
+//Adding a topping to the selected and removing from available
+function addTopping(topping) {
+  var index = listOfToppings.indexOf(topping);  //Finding Spot in array of topping
+  listOfToppings.splice(index, 1);  //Removing from list of available array
+  selectedToppings.push(topping); //Adding to list of selected array
+  thePizza.setToppings(selectedToppings); //Setting the object to the new toppings
+  var toppingLi = displayTopping(topping) // Generating new li
+  return toppingLi;  //returning Li
+};
+
+//Removing a topping from selected and adding to available.
+function removeTopping(topping){
+  var index = selectedToppings.indexOf(topping);  //Repeat of previous
+  selectedToppings.splice(index, 1);
+  listOfToppings.push(topping);
+  thePizza.setToppings(selectedToppings);
+  var toppingLi = displayTopping(topping);
+  return toppingLi;
+};
+
+$(document).ready(function(){
+
+  //Displaying all available toppings.
+  for (var i = 0; i < listOfToppings.length; i++){
+    $(".available").append(displayTopping(listOfToppings[i]));
+  };
+
+  //Click listener for selecting/deselecting toppings
+  $(".toppingUl").on('click', '.topping', function(){
+    var parentClasses = $(this).parent().attr("class");
+    var parentClass = parentClasses.split(" ");
+    var currentClasses = $(this).attr("class");
+    var currentClass = currentClasses.split(" ");
+    if (parentClass[0] == "available"){
+      toppingLi = addTopping(currentClass[0]);
+      $("." + currentClass[0]).remove();
+      $(".selected").append(toppingLi);
+    } else {
+      toppingLi = removeTopping(currentClass[0]);
+      $("." + currentClass[0]).remove();
+      $(".available").append(toppingLi);
+    }
+  });
+
+  $(".calculate").click(function(){
+    var theSize = $("#selectSize").val();
+    thePizza.setSize(theSize);
+    var theNum = $("#selectNum").val();
+    thePizza.setNum(theNum);
+    var calculate = thePizza.cost();
+    $(".the-cost").empty();
+    $(".the-cost").text(calculate);
+    var pizzaDescription = thePizza.description();
+    $(".order").text(pizzaDescription);
+  });
+
+});
---
+++ spec/specs.js
@@ -0,0 +1,78 @@
+describe("Pizza", function(){
+  it("After inputting a pizza, testPizza.prop should return the specified property", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni", "Sausage"]);
+    expect(testPizza.num).to.equal(2);
+    expect(testPizza.size).to.equal("Large");
+    expect(testPizza.toppings).to.eql(["Pepperoni", "Sausage"]);
+  });
+});
+
+describe("setNum", function(){
+  it("Expect a new number of pizzas to change the pizza number.", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni"]);
+    expect(testPizza.setNum(1)).to.eql(1);
+  });
+});
+
+describe("setSize", function(){
+  it("Expect a new size of the pizza to change the pizza size.", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni"]);
+    expect(testPizza.setSize("Small")).to.eql("Small");
+  });
+});
+
+describe("setToppings", function(){
+  it("Expect new toppings of the pizzas to change the pizza toppings.", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni"]);
+    expect(testPizza.setToppings(["Pepperoni", "Sausage"])).to.eql(["Pepperoni", "Sausage"]);
+  });
+});
+
+describe("describe", function(){
+  it("Expect a printout of the selected pizza with 1 topping", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni"]);
+    expect(testPizza.description()).to.eql("2 Large pizza(s) with Pepperoni");
+  });
+
+  it("Expect a printout of the selected pizza with 2 toppings", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni", "Sausage"]);
+    expect(testPizza.description()).to.eql("2 Large pizza(s) with Pepperoni and Sausage");
+  });
+
+  it("Expect a printout of the selected pizza with 3 toppings", function(){
+    var testPizza = new Pizza(2, "Large", ["Pepperoni", "Sausage", "Olive"]);
+    expect(testPizza.description()).to.eql("2 Large pizza(s) with Pepperoni, Sausage, and Olive");
+  });
+});
+
+describe("cost", function(){
+  it("Expect one small pizza with 1 topping to cost 6.25", function(){
+    var testPizza = new Pizza(1, "Small", ["Pepperoni"]);
+    expect(testPizza.cost()).to.eql(6.25);
+  });
+
+  it("Expect two medium pizzas with 2 topping to cost 20.00", function(){
+    var testPizza = new Pizza(2, "Medium", ["Pepperoni", "Sausage"]);
+    expect(testPizza.cost()).to.eql(22.00);
+  });
+  it("Expect three large pizzas with 3 topping to cost 50.25", function(){
+    var testPizza = new Pizza(3, "Large", ["Pepperoni", "Sausage", "Olive"]);
+    expect(testPizza.cost()).to.eql(50.25);
+  });
+});
+
+describe("addTopping", function(){
+  it("Expect to delete topping from list, and move to selected list, return Topping li", function(){
+    expect(addTopping("Pepperoni")).to.eql("<li class='Pepperoni topping'>Pepperoni</li>");
+    expect(listOfToppings).to.eql(["Sausage", "Ham", "Chicken", "Olive", "Onion", "Peppers", "Mushroom", "Tomatoes"]);
+    expect(selectedToppings).to.eql(["Pepperoni"]);
+  });
+});
+
+describe("removeTopping", function(){
+  it("Expect to delete topping from selected list, and move to topping list, return Topping li", function(){
+    expect(removeTopping("Pepperoni")).to.eql("<li class='Pepperoni topping'>Pepperoni</li>");
+    expect(listOfToppings).to.eql(["Sausage", "Ham", "Chicken", "Olive", "Onion", "Peppers", "Mushroom", "Tomatoes", "Pepperoni"]);
+    expect(selectedToppings).to.eql([]);
+  });
+});
